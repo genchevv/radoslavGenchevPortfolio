18a5fa33d0ec645045a229ec83e2a5f8d2e64013
Radoslav Genchev <r.genchew@gmail.com>
2017-06-19 12:48:35 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+gem 'genchew_view_tool', git: 'https://github.com/genchevv/genchew_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end    
     end
+    
+    def copyright_generator
+       GenchewViewTool::Renderer.copyright 'Radoslav Genchev', 'All rights reserved' 
+    end
 end
