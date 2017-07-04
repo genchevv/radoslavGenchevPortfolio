7127b301d7938bcc8e76b92f173275250cdde66e
Radoslav Genchev <r.genchew@gmail.com>
2017-07-04 14:12:24 +0000

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Workspace
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
